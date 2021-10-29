add30e2c40bbc8fe3e0cc4a3218ac113644d9337
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2021-10-29 10:57:48 +0100

    first commit with hello world project

---
+++ hello-world/src/main/java/Main.java
@@ -0,0 +1,26 @@
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class Main {
+    public static void main(String[] args) {
+        try (ZContext context = new ZContext()) {
+            // Socket to talk to clients
+            ZMQ.Socket socket = context.createSocket(ZMQ.REP);
+            socket.bind("tcp://localhost:5555");
+
+            while (!Thread.currentThread().isInterrupted()) {
+                // Block until a message is received
+                byte[] reply = socket.recv(0);
+
+                // Print the message
+                System.out.println(
+                        "Received: [" + new String(reply, ZMQ.CHARSET) + "]"
+                );
+
+                // Send a response
+                String response = "Hello, world!";
+                socket.send(response.getBytes(ZMQ.CHARSET), 0);
+            }
+        }
+    }
+}
---
+++ hello-world/src/main/java/Test.java
@@ -0,0 +1,31 @@
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        try (ZContext context = new ZContext()) {
+            // Socket to talk to clients
+            ZMQ.Socket socket = context.createSocket(ZMQ.REQ);
+            socket.connect("tcp://localhost:5555");
+
+            String message;
+
+            do {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("New Message: ");
+
+                message = scanner.nextLine();
+                socket.send(message.getBytes(ZMQ.CHARSET), 0);
+                byte[] reply = socket.recv(0);
+                System.out.println(
+                        "Received: [" + new String(reply, ZMQ.CHARSET) + "]"
+                );
+            } while (!message.equals("DONE"));
+
+            socket.close();
+        }
+    }
+}
